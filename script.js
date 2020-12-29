48f19c5281bdc7abc5d09b8515550895a62880ce
Ywette <65426230+Ywette@users.noreply.github.com>
2020-12-29 15:30:52 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,42 +1,34 @@
 function submit(){
     
-  validateEmpty();
-  validateCo(); 
   validateEmail();
+  validateCo();   
   validateCheck(); 
-  
-    
+  validateEmpty();
+      
 }
-// runSuccessMsg();
 
 
 function validateEmpty (){
   if (document.getElementById('email').value ===""){
     document.getElementById('emailError').innerText = "Provide email";
     document.getElementById('emailError').className = "error";
+    return false;
   }
 }
 
 
 function validateCo(){
   if (document.getElementById('email').value.endsWith(".co")){       
-    // alert("YESSSSSSSSSSSSS");
+    // alert("ends with .co");
     document.getElementById('emailError').innerText = "We are not accepting subscriptions from Columbia";
     document.getElementById('emailError').className = "error";
-    return false;
-
-  }  else {
-      // alert("NOOOOOOOOOO");
-      document.getElementById('emailError').style.display = "none";
-      return true;
   }
 }
 
 function validateEmail(mail){ 
- if (/^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/.test(document.getElementById('email').value)
-    && (document.getElementById('email').value !== "")){
-    // alert("You have entered an VALID email address!")
-    document.getElementById('emailError').style.display = "none";
+ if (/^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/.test(document.getElementById('email').value))
+    {
+    document.getElementById('emailError').innerText = "";
     return true;
   }
     document.getElementById('emailError').innerText = "Please provide a valid e-mail address";
@@ -46,25 +38,22 @@ function validateEmail(mail){
 
 function validateCheck(){
   if (document.getElementById('check').checked){
-    document.getElementById('checkboxError').style.display = "none";
-    // alert("YESSSSSSSSSSSSS")
+    document.getElementById('checkboxError').innerText = "";
+    return true;
   } else {   
       // alert("NOOOOOOOOOO");
-      document.getElementById('checkboxError').innerText = "You must accept the terms and conditions";
-      document.getElementById('checkboxError').className = "error";
-      return false;
+    document.getElementById('checkboxError').innerText = "You must accept the terms and conditions";
+    document.getElementById('checkboxError').className = "error";
+    return false;
   }
 } 
 
 function runSuccessMsg() {  
 
-  // alert('SUCCESS');
-  if (validateEmpty() == true && validateCo() == true && validateEmail() == true && validateCheck() == true){
-
   document.getElementsByClassName('success-logo')[0].style.display = "inline";
-  // document.getElementById('emailError').style.display = "none";    
+  document.getElementById('emailError').style.display = "none";    
   document.getElementById('main-thought').innerText = "Thanks for subscribing!"    
   document.getElementsByClassName('subheading')[0].innerText = "You have successfully subscribed to our email listing. Check your email for the discount code."
   document.getElementsByClassName('form-container')[0].style.display = "none";    
-  }
+  
 }
