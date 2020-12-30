776c073c44fee2099256fe4d33f2daf45b2b2a43
Ywette <65426230+Ywette@users.noreply.github.com>
2020-12-30 16:51:41 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,31 +1,42 @@
-function submit(){
-    
-  validateEmail();
-  validateCo();   
-  validateCheck(); 
-  validateEmpty();
-      
+submit = () => {
+  
+  let validEmail = validateEmail();
+  let validCo = validateCo();   
+  let validCheck = validateCheck(); 
+  let validEmpty = validateEmpty();     
+  
+  if ((validEmail === true) && 
+    (validCo === true) && 
+    (validCheck === true) && 
+    (validEmpty === true)){
+    runSuccessMsg();
+  }
 }
 
 
-function validateEmpty (){
+validateEmpty = () => {
+
   if (document.getElementById('email').value ===""){
     document.getElementById('emailError').innerText = "Provide email";
     document.getElementById('emailError').className = "error";
     return false;
   }
-}
-
-
-function validateCo(){
-  if (document.getElementById('email').value.endsWith(".co")){       
-    // alert("ends with .co");
-    document.getElementById('emailError').innerText = "We are not accepting subscriptions from Columbia";
-    document.getElementById('emailError').className = "error";
+  else {
+    return true;
   }
 }
 
-function validateEmail(mail){ 
+validateCo= ()=>{
+  if (document.getElementById('email').value.endsWith(".co")){       
+    document.getElementById('emailError').innerText = "We are not accepting subscriptions from Columbia";
+    document.getElementById('emailError').className = "error";
+    return false
+  }else{
+    return true
+  }
+}
+
+validateEmail = (mail) => { 
  if (/^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/.test(document.getElementById('email').value))
     {
     document.getElementById('emailError').innerText = "";
@@ -36,7 +47,7 @@ function validateEmail(mail){
     return false;
 }
 
-function validateCheck(){
+validateCheck = () => {
   if (document.getElementById('check').checked){
     document.getElementById('checkboxError').innerText = "";
     return true;
@@ -48,12 +59,11 @@ function validateCheck(){
   }
 } 
 
-function runSuccessMsg() {  
-
-  document.getElementsByClassName('success-logo')[0].style.display = "inline";
-  document.getElementById('emailError').style.display = "none";    
+runSuccessMsg = () => {  
+  
+  document.getElementsByClassName('success-logo')[0].style.display = "inline-block";    
   document.getElementById('main-thought').innerText = "Thanks for subscribing!"    
   document.getElementsByClassName('subheading')[0].innerText = "You have successfully subscribed to our email listing. Check your email for the discount code."
-  document.getElementsByClassName('form-container')[0].style.display = "none";    
-  
+  document.getElementsByClassName('form-container')[0].style.display = "none"; 
+  document.getElementById('emailError').style.display = "none";       
 }
